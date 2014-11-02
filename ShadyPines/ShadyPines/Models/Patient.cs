8c14ff4a4437f9edb2345b760f48eef2444f1089
GaryCurran <garycurran@live.com>
2014-11-02 12:22:08 +0000

    First Draft

---
+++ ShadyPines/ShadyPines/Models/Patient.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShadyPines.Models
+{
+    public class Patient
+    {
+        String PatientID { get; set; }
+
+    }
+}
